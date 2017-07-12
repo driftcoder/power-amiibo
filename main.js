35d4e58d95b1e4df8838a30fb1c08b677c08830f
driftcoder <_>
2017-07-12 09:23:17 -0500

    Added missing files

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var child_process = require('child_process');
 var fs = require('fs');
 
 var HID = require('node-hid');
@@ -33,7 +34,7 @@ try {
 }
 
 setLedBrightness(device, 0);
-console.log('Waiting for', command == 'read' ? 'Amiibo' : 'tag', '...');
+console.log('Waiting for tag ...');
 
 setInterval(() => {
   var status = getStatus(device);
@@ -42,11 +43,11 @@ setInterval(() => {
     return;
   }
 
-  console.log(command == 'read' ? 'Amiibo' : 'tag', 'found');
+  console.log('Tag found');
   console.log();
 
-  command == 'read' && writeAmiiboToFile(device, path);
-  command == 'write' && writeAmiiboToTag(device, path);
+  command == 'read' && readTag(device, path);
+  command == 'write' && writeTag(device, path);
 
   quit('Done.');
 }, 1000);
@@ -55,8 +56,8 @@ function printUsage() {
   console.log('Usage: node main.js <command> <path>');
   console.log('');
   console.log('Commands:');
-  console.log('    read     Reads Amiibo and writes it to a file.');
-  console.log('    write    Reads a file and writes it to a tag.');
+  console.log('    read');
+  console.log('    write');
 }
 
 function quit(message = null) {
@@ -80,7 +81,7 @@ function setLedBrightness(device, value) {
   device.write(getCommand([0x20, value]));
 }
 
-function writeAmiiboToFile(device, path) {
+function readTag(device, path) {
   var data = [];
 
   //34 16bytes pages
@@ -105,15 +106,62 @@ function writeAmiiboToFile(device, path) {
   console.log();
 }
 
-function writeAmiiboToTag(device, path) {
-  var data = fs.readFileSync(path);
+function writeTag(device, path) {
+  var data = [];
+
+  device.write(getCommand([0x1C, 0]));
+  var serial = device.readSync().slice(2, 18);
+
+  // Change the serial of the bin file
+  child_process.execFileSync('amiitool', ['-d', '-k', 'retail.bin', '-i', path, '-o', 'temp.bin']);
+  data = fs.readFileSync('temp.bin');
+  data[468] = serial[0];
+  data[469] = serial[1];
+  data[470] = serial[2];
+  data[471] = serial[3];
+  data[472] = serial[4];
+  data[473] = serial[5];
+  data[474] = serial[6];
+  data[475] = serial[7];
+  fs.writeFileSync('temp.bin', data);
+
+  child_process.execFileSync('amiitool', ['-e', '-k', 'retail.bin', '-i', 'temp.bin', '-o', 'signed.bin']);
+
+  data = fs.readFileSync('signed.bin');
   data = Array.prototype.slice.call(data, 0);
 
-  // 135 4byte pages
-  for (i = 0; i <= 134; i++) {
+  // Write the main data
+  for (i = 3; i <= 129; i++) {
     device.write(getCommand([0x1D, i].concat(data.slice(i * 4, (i * 4) + 4))));
-    console.log(device.readSync().toString('hex'));
+    device.readSync();
   }
+
+  // Write pack
+  device.write(getCommand([0x1D, 134, 0x80, 0x80, 0x00, 0x00]));
+  device.readSync();
+
+  // Write password
+  device.write(getCommand([0x1D, 133].concat(getPassword(serial))));
+  device.readSync();
+
+  // Write locks
+  device.write(getCommand([0x1D, 2, serial[8], serial[9], 0x0F, 0xE0]));
+  device.readSync();
+  device.write(getCommand([0x1D, 130, 0x01, 0x00, 0x0F, 0x00]));
+  device.readSync();
+  device.write(getCommand([0x1D, 131, 0x00, 0x00, 0x00, 0x04]));
+  device.readSync();
+  device.write(getCommand([0x1D, 132, 0x5F, 0x00, 0x00, 0x00]));
+  device.readSync();
+}
+
+function getPassword(serial) {
+  return [
+    serial[1] ^ serial[4] ^ 0xAA,
+    serial[2] ^ serial[5] ^ 0x55,
+    serial[4] ^ serial[6] ^ 0xAA,
+    serial[5] ^ serial[7] ^ 0x55,
+  ];
 }
 
 function getCommand(command) {
